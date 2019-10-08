4c008bbc1e46c2b6693c87b2ff06b4566d30b470
Arian Gagica <gagicaarian@gmail.com>
2019-10-08 18:23:54 +0200

    favicon+new anim+sorted out queue

--- show-on-scroll.js
+++ show-on-scroll.js
@@ -36,3 +36,25 @@ function isElementInViewport(el) {
 		(rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
 	);
 }
+
+//Get the button:
+mybutton = document.getElementById('myBtn');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
